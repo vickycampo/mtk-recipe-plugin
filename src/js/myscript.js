abf6791f64828a8d436af16e9e11bfc4f0701e89
Vicky Campo <vickycampo@gmail.com>
2019-05-28 08:13:13 +0200

    Bug Fixing

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -64,8 +64,13 @@ jQuery(document).ready(function ($) {
 			multiple: false
           });
           file_frame.on('select', function() {
-          			var attachment = file_frame.state().get('selection').first().toJSON();
-          			$button.siblings('.image-upload').val(attachment.url);
+			var attachment = file_frame.state().get('selection').first().toJSON();
+			$button.siblings('.image-upload').val(attachment.url);
+               /*  Enable the Widget save button */
+               $('.widget-control-save' ,'.widget-control-actions' ).val('Save');
+               $('.widget-control-save' , '.widget-control-actions').attr("disabled", false);
+               console.log ('here');
+
           });
 
           file_frame.open();
