76f21c54dfc12c86ca2821566c7a54e4eb57df1d
Vicky Campo <vickycampo@gmail.com>
2019-05-28 21:53:50 +0200

    Part 39 - b

--- inc/Base/TestimonialController.php
+++ inc/Base/TestimonialController.php
@@ -51,35 +51,68 @@ class TestimonialController extends BaseController
 	}
      public function add_meta_boxes()
      {
+          /* Author Name */
           add_meta_box(
-               'testimonial_author',
-               'Author',
-               array ( $this , 'render_author_box'),
+               'testimonial_options',
+               'Testimonial Options',
+               array ( $this , 'render_features_box'),
                'testimonial',
                'side',
                'default'
           );
+          /* Author email */
+          /* approved [checkbox] */
+          /* featured [checkbox] */
+
 
 
      }
 
-     public function render_author_box ( $post )
+     public function render_features_box ( $post )
      {
 
           /*  validate that the contents of the form request */
           wp_nonce_field( 'mtk_testimonial_author' , 'mtk_testimonial_author_nonce' );
-          $value = get_post_meta ( $post->ID , '_mtk_testimonial_author_key' , true );
+          /* Get the data */
+          $data  = get_post_meta ( $post->ID , '_mtk_testimonial_author_key' , true );
+          /* Create the variables where we are going to sort the information */
+          /* Author Name */
+          $name = isset($data['name']) ? $data['name'] : '';
+          /* Email */
+          $email = isset($data['email']) ? $data['email'] : '';
+          /* approved */
+          $approved = isset($data['approved']) ? $data['approved'] : false;
+          /* featured */
+          $featured = isset($data['featured']) ? $data['featured'] : false;
           ?>
-          <label for="mtk_testimonial_author">
-               Testimonial Author
-          </label>
-          <input type="text" id="mtk_testimonial_author" name="mtk_testimonial_author" value="<?php echo ( esc_attr( $value ) ); ?>"
+          <label class="meta-label" for="mtk_testimonial_author">Author Name</label>
+          <input type="text" id="mtk_testimonial_author" name="mtk_testimonial_author" value="<?php echo ( esc_attr( $name ) ); ?>"
+          <label class="meta-label" for="mtk_testimonial_author">Author Email</label>
+          <input type="text" id="mtk_testimonial_author" name="mtk_testimonial_email" value="<?php echo ( esc_attr( $name ) ); ?>"
+          <div class="meta-container">
+			<label class="meta-label w-50 text-left" for="mtk_testimonial_approved">Approved</label>
+			<div class="text-right w-50 inline">
+				<div class="ui-toggle inline"><input type="checkbox" id="mtk_testimonial_approved" name="mtk_testimonial_approved" value="1" <?php echo $approved ? 'checked' : ''; ?>>
+					<label for="mtk_testimonial_approved"><div></div></label>
+				</div>
+			</div>
+		</div>
+          <div class="meta-container">
+			<label class="meta-label w-50 text-left" for="mtk_testimonial_featured">Featured</label>
+			<div class="text-right w-50 inline">
+				<div class="ui-toggle inline"><input type="checkbox" id="mtk_testimonial_featured" name="mtk_testimonial_featured" value="1" <?php echo $featured ? 'checked' : ''; ?>>
+					<label for="mtk_testimonial_featured"><div></div></label>
+				</div>
+			</div>
+		</div>
           <?php
      }
      public function save_meta_box( $post_id )
      {
           /* Happens every time the user saves the post */
-
+          echo ('<pre>');
+          print_r ($_POST);
+          echo ('</pre>');
           if ( ! ( isset ( $_POST['mtk_testimonial_author_nonce'] ) ) )
           {
                /* If the another post is saved, not the testimonial type then we just return the post id*/
@@ -89,7 +122,7 @@ class TestimonialController extends BaseController
           /* We store our custom nonce */
           $nonce = $_POST['mtk_testimonial_author_nonce'];
           /* Verify the that the none is valid */
-          if ( ! ( wp_verify ( $nonce , 'mtk_testimonial_author' ) ) )
+          if ( ! ( wp_verify_nonce ( $nonce , 'mtk_testimonial_author' ) ) )
           {
                /* if is not valid we stop the execution of the script */
                return ( $post_id );
@@ -102,8 +135,16 @@ class TestimonialController extends BaseController
           /* Check it the user has the ability to edit the post */
           if ( ! ( current_user_can ( 'edit_post' , $post_id ) ) )
           {
-               
+               return ( $post_id );
           }
+          /* Store this metabox */
+          $data = array(
+			'name' => sanitize_text_field( $_POST['mtk_testimonial_author'] ),
+			'email' => sanitize_text_field( $_POST['mtk_testimonial_email'] ),
+			'approved' => isset($_POST['mtk_testimonial_approved']) ? 1 : 0,
+			'featured' => isset($_POST['mtk_testimonial_featured']) ? 1 : 0,
+		);
+          update_post_meta( $post_id, '_mtk_testimonial_key', $data );
      }
 
 }
