be60a97020cd08787eabea13191a63ffdfaa684a
Vicky Campo <vickycampo@gmail.com>
2019-06-10 07:45:51 +0200

    Part 45

--- src/js/form.js
+++ src/js/form.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded' , function (e)
-{
-     console.log ('Ready');
-}
+     {
+          /* We need to prevent the form submit */
+          let testimonialForm = document.getElementById('mtk-testimonial-form');
+          testimonialForm.addEventListener( 'submit' , (e) => {
+               e.preventDefault();
+               console.log( 'Prevent form to submit' );
+               //Reset the form Messeges
+               //Validate Email address
+               //collect all the data
+               //ajax http post request
+          } );
+     }
 );
